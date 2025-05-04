ade2ce519edce157d2fdb4c195603cb032b79378
WANESSA MARGARIDA DE OLIVEIRA <wanmoli1@hotmail.com>
2025-05-03 21:55:42 -0300

    Create trunfo.c

---
+++ trunfo.c
@@ -0,0 +1,102 @@
+#include<stdio.h> 
+int main()// carta 1
+{   char estado[50];
+    char nome da cidade [50];
+    char regiao[50];
+    char  sigla [4];
+    int popualacao;
+    float area; km
+    float densidade;
+    float pib;
+    int pontos turisticos;
+
+        printf("Digite o nome do estado: %s", estado);
+            scanf("%s", estado);
+
+        printf("Digite o nome da cidade: %s", nome da cidade);
+            scanf("%s", nome da cidade);
+
+        printf("Digite a regiao: %s", regiao);
+            scanf("%s", regiao);
+
+        printf("Digite a sigla do estado: %s", sigla);
+            scanf("%s", sigla);
+
+        printf("Digite a populacao: %d", popualacao);
+            scanf("%d", &popualacao);
+
+        printf("Digite o pib: %f", pib);
+            scanf("%f", &pib);
+
+        printf("Digite o numero de pontos turisticos: %d", pontos turisticos);
+            scanf("%d", &pontos turisticos);
+
+        printf("Digite a area: %f", area);
+            scanf("%f", &area);
+
+        printf("Digite a densidade: %f", densidade);
+            scanf("%f", &densidade);
+
+                printf("Estado: %s\n", estado);
+                printf("Cidade: %s\n", nome da cidade);
+                printf("Regiao: %s\n", regiao);
+                printf("Sigla: %s\n", sigla);
+                printf("Populacao: %d\n", popualacao);
+                printf("Pib: %f\n", pib);
+                printf("Pontos turisticos: %d\n", pontos turisticos);
+                printf("Area: %f\n", area);
+                printf("Densidade: %f\n", densidade);
+
+        return 0;
+}
+    int main()// carta 2
+    {   char estado[50];
+    char nome da cidade [50];
+    char regiao[50];
+    char  sigla [4];
+    int popualacao;
+    float area; km
+    float densidade;
+    float pib;
+    int pontos turisticos;
+
+        printf("Digite o nome do estado: %s", estado);
+            scanf("%s", estado);
+
+        printf("Digite o nome da cidade: %s", nome da cidade);
+            scanf("%s", nome da cidade);
+
+        printf("Digite a regiao: %s", regiao);
+            scanf("%s", regiao);
+
+        printf("Digite a sigla do estado: %s", sigla);
+            scanf("%s", sigla);
+
+        printf("Digite a populacao: %d", popualacao);
+            scanf("%d", &popualacao);
+
+        printf("Digite o pib: %f", pib);
+            scanf("%f", &pib);
+
+        printf("Digite o numero de pontos turisticos: %d", pontos turisticos);
+            scanf("%d", &pontos turisticos);
+
+        printf("Digite a area: %f", area);
+            scanf("%f", &area);
+
+        printf("Digite a densidade: %f", densidade);
+            scanf("%f", &densidade);
+                    
+                    printf("Estado: %s\n", estado);
+                    printf("Cidade: %s\n", nome da cidade);
+                    printf("Regiao: %s\n", regiao);
+                    printf("Sigla: %s\n", sigla);
+                    printf("Populacao: %d\n", popualacao);
+                    printf("Pib: %f\n", pib);
+                    printf("Pontos turisticos: %d\n", pontos turisticos);
+                    printf("Area: %f\n", area);
+                    printf("Densidade: %f\n", densidade);
+                    
+            return 0;
+
+    }
